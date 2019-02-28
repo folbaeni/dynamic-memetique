7e1a12cd1c7fb6b8f650bc9038440c7135909094
VetFalco <folco@bertinibaldassini.com>
2019-02-28 18:58:58 +0100

    corrections

--- Code/basic-network.py
+++ Code/basic-network.py
@@ -10,27 +10,40 @@ def generation_reseau(numero_gens):
 
 def probabilite_numero_amis(network_name):
     gens= len(network_name.nodes.data())
-    x = np.random.randint(gens)
-    return x
+    x = np.random.randint(1,gens,size=10)
+    t=gens
+    for i in x:
+        if i < t:
+            t=i
+    return t
+
+def numero_amis(network_name, personne):
+    r=0
+    for e,_ in network_name.edges.items():
+        f,g=e
+        if f==personne:
+            r+=1
+    return r
+
 
 def edges_newnetwork(network_name):
-    #Il faut ajouter la probabilité de numero amis
     gens= len(network_name.nodes.data())
     boucle_temp= copy.deepcopy(network_name.nodes.data())
     for (i,_) in boucle_temp:
-        x = probabilite_numero_amis(network_name) # - numero amis qu'il a
-        for j in range(x):
-            network_name.add_edge(i,np.random.randint(gens))
+        x = probabilite_numero_amis(network_name) - numero_amis(network_name,i)
+        if x>0:
+            for j in range(x):
+                network_name.add_edge(i,np.random.randint(gens))
     return()
 
+#INSERTION PARAMETRES INITIALES
+grandeur_network = 50
 
-NETWORK = generation_reseau(10)
+#FONCTIONS APPELLES
+NETWORK = generation_reseau(grandeur_network)
 edges_newnetwork(NETWORK)
 
-
-# numphy.random.choice
-#random randint
-
+#DESSIN GRAPHE
 nx.draw(NETWORK, with_labels=True)
 plt.draw()
 plt.show()
